1840b836908c42991b1d0f90d7d85e908677f810
Gonzalo Migliardi <gonzalomigliardi9@gmail.com>
2022-08-31 17:58:23 -0300

    corregido

--- src/App.js
+++ src/App.js
@@ -1,31 +1,8 @@
 import "bootstrap/dist/css/bootstrap.css";
-
+import Navbar from './components/NavBar';
 const App = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Drink Shop</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Alcohol</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Cervezas</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Vinos</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Sin Alcohol</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+    <Navbar/>
   );
 }
 
